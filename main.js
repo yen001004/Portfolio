417866154e8064bfb3674fa18edb7033ba5d94f6
ye-ong <yenn001004@gmail.com>
2021-02-05 12:15:44 +0900

    JS : Work filtering

--- main.js
+++ main.js
@@ -50,6 +50,29 @@ arrowUp.addEventListener('click', () => {
     scrollIntoView('#home');
 });
 
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null) {
+        return;
+    }
+    projectContainer.classList.add('anima-out');
+
+    setTimeout(() => {
+        projects.forEach((project) => {
+            if(filter ==='*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            }
+            else {
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anima-out');
+    }, 300);
+});
 
 
 
