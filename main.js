8c2affaa9764fca3e109ba09df2ed7f39131b2e5
ye-ong <yenn001004@gmail.com>
2021-02-05 11:16:48 +0900

    JS : Transparent

--- main.js
+++ main.js
@@ -28,6 +28,18 @@ homeContact.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
 
+// Make home slowly fade to transparent as the window scrolls down
+const hoem = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity=1-window.scrollY / homeHeight;
+});
+
+
+
+
+
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: "smooth"});
