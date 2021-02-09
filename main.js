a80ac4f7a4440c01d0a365d322eedb7894539ab4
ye-ong <yenn001004@gmail.com>
2021-02-09 13:24:01 +0900

    Reeaal final maybe

--- main.js
+++ main.js
@@ -91,7 +91,57 @@ workBtnContainer.addEventListener('click', (e) => {
 
 
 
+
+
+const sectionIds = ['#home', '#about', '#skills', '#work', '#testimonials', '#contact',];
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => 
+    document.querySelector(`[data-link="${id}"]`));
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: "smooth"});
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
+
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3,
+}
+const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting && entry.intersectionRatio > 0) {
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            
+            // scroll down page up
+            if(entry.boundingClientRect.y < 0) {
+                selectedNavIndex = index + 1;
+            }
+            else {
+                selectedNavIndex = index - 1;
+            }
+            
+        }
+    });
+};
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('wheel', () => {
+    if (window.scroolY === 0) {
+        selectedNavIndex = 0;
+    }
+    else if (Math.round(window.scrollY + window.innerHeight) >= document.body.clientHeight) {
+        selectedNavIndex = navItems.length - 1;
+    }
+    selectNavItem(navItems[selectedNavIndex]);
+})
